19cbf3013eed30a4f16a19c59a070895ed3752d9
Vinicio Ferrari <vinicio@backupautomacao.com.br>
2023-02-02 14:59:31 -0300

    Soma de dois números fixos

---
+++ src/main/kotlin/Soma.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val primeiroNumero = 2.0  //Define que a variável "primeiroNumero" será 2.0 (tipo da variável Double)
+    val segundoNumero = 8.0  //Define que a variável "segundoNumero" será 8.0 (tipo da variável Double)
+    val soma= primeiroNumero + segundoNumero  //Define que a variável "soma" será a soma das duas variáveis
+    println("A soma de $primeiroNumero mais $segundoNumero é: $soma")
+
+}
