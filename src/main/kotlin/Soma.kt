16009499436e27dfe6829f557884dba154dce6b2
Vinicio Ferrari <vinicio@backupautomacao.com.br>
2023-02-02 16:58:15 -0300

    O usuário informa dois numeros e em seguida é realizda a soma dos numeros informados.

--- src/main/kotlin/Soma.kt
+++ src/main/kotlin/Soma.kt
@@ -1,7 +1,9 @@
-fun main(){
-    val primeiroNumero = 2.0  //Define que a variável "primeiroNumero" será 2.0 (tipo da variável Double)
-    val segundoNumero = 8.0  //Define que a variável "segundoNumero" será 8.0 (tipo da variável Double)
-    val soma= primeiroNumero + segundoNumero  //Define que a variável "soma" será a soma das duas variáveis
+fun main() {
+    print("Digite um número: ") //Imprimi "Digite um número: "
+    val primeiroNumero = readln().toDouble() //Aguarda o usuário inserir um número
+    print("Digite outro número: ") //Imprimi "Digite outro número: "
+    val segundoNumero = readln().toDouble() //Aguarda o usuário inserir um número
+    val soma = String.format("%.2f",primeiroNumero + segundoNumero) // Realiza a formatação para duas casas depois da virgula do resultado da soma das duas variáveis acima,
     println("A soma de $primeiroNumero mais $segundoNumero é: $soma")
 
 }
